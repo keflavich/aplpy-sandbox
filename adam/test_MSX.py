e63d974c89bd51cc48af135da7fcf007108719c9
Adam Ginsburg <adam.g.ginsburg@gmail.com>
2012-02-29 16:00:35 -0700

    did a little more with image tests; one currently fails (don't tihnk it should - without data, frame is not generated)

--- adam/test_MSX.py
+++ adam/test_MSX.py
@@ -1,15 +1,37 @@
 import matplotlib
 import aplpy
 import os
+import numpy as np
+import pytest
+import pyfits
 
-filename = 'D1740P30.FIT'
-if os.path.exists('D1740P30.FIT'):
-    F = aplpy.FITSFigure('D1740P30.FIT')
-    F.show_grayscale()
-else:
-    import urllib
-    import pyfits
-    ff = pyfits.open(urllib.urlretrieve('http://eta.colorado.edu/aplpy-debug/%s' % filename)[0])
-    F = aplpy.FITSFigure(ff)
-    F.show_grayscale()
+# Test initialization 
+@pytest.mark.parametrize(('data'), [np.arange(256).reshape((16, 16)), None])
+def test_init(data):
+    header = pyfits.Header()
+    header.fromTxtFile('MSX.hdr')
+    HDU = pyfits.PrimaryHDU(data=data, header=header)
+    f = aplpy.FITSFigure(HDU)
+    suffix = 'withdata' if data is not None else 'nodata'
+    f.save('MSX_frame_%s.png' % suffix)
+    f.close()
+    print "Ran test MSX_frame_%s" % suffix
+                        
+
+
+# Test simple contour generation with Numpy example (but real, MSX header)
+@pytest.mark.parametrize(('filled'), [True, False])
+def test_numpy_contour(filled):
+    data = np.arange(256).reshape((16, 16))
+    header = pyfits.Header()
+    header.fromTxtFile('MSX.hdr')
+    HDU = pyfits.PrimaryHDU(data=data, header=header)
+    f = aplpy.FITSFigure(HDU)
+    f.show_grayscale()
+    f.show_contour(HDU, levels=np.linspace(1., 254., 10), filled=filled)
+    suffix = 'filled' if filled else 'notfilled'
+    f.save('MSX_contours_%s.png' % suffix)
+    f.close()
+    print "Ran test %s" % suffix
+                        
 
